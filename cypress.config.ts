d6f4f4e6b729559f95a1c52e619148c3860473c1
Raygen Rupe <raygenrrupe@gmail.com>
2024-08-30 21:33:48 -0600

    setup test:e2e with cypress, fix --force-reset, connect to cypress cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: '1xne6k',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
