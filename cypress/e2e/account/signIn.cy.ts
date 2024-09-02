309f4c30be88d823f0dabf53c8e558dbe2731bab
Raygen Rupe <raygenrrupe@gmail.com>
2024-09-01 23:59:11 -0600

    record cypress test, use the right test account.

--- cypress/e2e/account/signIn.cy.ts
+++ cypress/e2e/account/signIn.cy.ts
@@ -1,31 +1,34 @@
-import { Account } from '../../../shared/db-types';
 import { SignInPage } from '../../pages/account/signInPage';
 
 describe('Sign In Page', () => {
     const signInPage = new SignInPage();
+
+    const user = {
+        username: 'testuser',
+        password: 'T3stP4ssword!',
+        email: 'testmail@example.com',
+        firstName: 'Test',
+        lastName: 'User'
+    };
 
     beforeEach(() => {
         signInPage.visit();
     });
 
     before(() => {
-        it('should generate a verified account', () => {
-            cy.task('generateVerifiedAccount', {
-                username: 'testuser',
-                password: 'T3stP4ssword!',
-                email: 'testuser@example.com',
-                firstName: 'Test',
-                lastName: 'User'
-            });
-            console.log('Account created');
+        cy.task('generateVerifiedAccount', {
+            username: user.username,
+            password: user.password,
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName
         });
+        cy.log('Account created');
     });
 
     after(() => {
-        it('should remove the account', () => {
-            cy.task('removeAccountFromUsername', { username: 'testuser' });
-            console.log('Account removed');
-        });
+        cy.task('removeAccountFromUsername', { username: 'testuser' });
+        cy.log('Account removed');
     });
 
     it('should display the correct title', () => {
@@ -54,20 +57,22 @@ describe('Sign In Page', () => {
         cy.url().should('include', '/account/sign-up');
     });
 
-    it('should handle reset password click', () => {
-        signInPage.clickResetPassword();
-        // need to add the reset password prompt
-    });
+    describe('Reset Password', () => {
+        it('should handle reset password click', () => {
+            signInPage.clickResetPassword();
+            // need to add the reset password prompt
+        });
 
-    it('should handle cancel reset password', () => {
-        signInPage.clickResetPassword();
-        signInPage.cancelResetPassword();
+        it('should handle cancel reset password', () => {
+            signInPage.clickResetPassword();
+            signInPage.cancelResetPassword();
+        });
     });
 
     it('should successfully submit the form and redirect to home page', () => {
-        signInPage.fillForm('testuser', 'T3stP4ssword!');
+        signInPage.fillForm(user.username, user.password);
         signInPage.submitForm();
         cy.url().should('eq', 'http://localhost:3000/home');
-        cy.task('removeAccountFromUsername', { username: 'testuser' });
+        cy.task('removeAccountFromUsername', { username: user.username });
     });
 });
